b6ad2cb88ef0a676f1b76b59c2e6d21a824a3393
samutamm <samutamm@gmail.com>
2015-09-14 10:09:17 +0200

    Init specs.
    Server skeleton.

---
+++ src/backend/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var Percolator = require('percolator').Percolator;
+var dbSession = require('../../src/backend/dbSession.js');
+
+var port = 8080;
+var server = Percolator({'port': port, 'autoLink': false});
+
+server.route('/api/keywords',
+  {
+    GET: function(req, res) {
+      dbSession.fetchAll(
+        'SELECT id, value, categoryID FROM keyword ORDER BY id',
+        function(err, rows) {
+          if (err) {
+            console.log(err);
+            res.status.internalServerError(err);
+          } else {
+            console.log(rows);
+            res.collection(rows).send();
+          }
+        }
+      );
+    }
+  }
+);
+
+server.listen(function() {
+  console.log('Server started and listening on port', port);
+});
