d881504891235e201634ada1797e96f9e6a773c9
samutamm <samutamm@gmail.com>
2015-09-16 22:31:25 +0200

    Automate database creating by migrations.

--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -2,10 +2,30 @@
 
 var request = require('request');
 var dbSession = require('../../src/backend/dbSession.js');
+var Server = require('../../src/backend/server.js').Server;
 var resetDatabase = require('../resetDatabase.js');
 var async = require('async');
 
 describe('The API', function() {
+
+  var server;
+
+  beforeEach(function(done) {
+    server = Server('8081');
+    server.listen(function(err) {
+      resetDatabase(dbSession, function() {
+        done(err);
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    server.close(function() {
+      resetDatabase(dbSession, function() {
+        done();
+      })
+    });
+  });
 
   it('should respond to a GET request at /api/keywords/', function(done) {
     var expected = {
@@ -53,7 +73,7 @@ describe('The API', function() {
       function(err, results) {
         request.get(
           {
-            'url': 'http://localhost:8080/api/keywords/',
+            'url': 'http://localhost:8081/api/keywords/',
             'json': true
           },
           function(err, res, body) {
